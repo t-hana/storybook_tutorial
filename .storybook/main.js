d525417c4f83e1a57a9b4d93a2cd773132e55717
花房智明 <hanafusa.tomoaki@a-tm.co.jp>
2023-08-24 10:40:27 +0900

    yarn storyboo 可能に修正
    コンポーネント完成
    
    fix accessibility

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,19 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.jsx"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-a11y",
   ],
+  features: {
+    postcss: false,
+  },
   framework: {
     name: "@storybook/react-vite",
     options: {},
@@ -14,5 +21,6 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  core: {},
 };
 export default config;
