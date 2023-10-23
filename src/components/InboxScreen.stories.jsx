85191f0913551b6dba05833669918dabe6784e40
花房智明 <hanafusa.tomoaki@a-tm.co.jp>
2023-10-23 15:36:00 +0900

    インタラクションテスト

--- src/components/InboxScreen.stories.jsx
+++ src/components/InboxScreen.stories.jsx
@@ -6,6 +6,13 @@ import { rest } from "msw";
 import { MockedState } from "./TaskList.stories";
 import { Provider } from "react-redux";
 
+import {
+  fireEvent,
+  within,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@storybook/testing-library";
+
 export default {
   component: InboxScreen,
   title: "InboxScreen",
@@ -26,6 +33,19 @@ Default.parameters = {
       ),
     ],
   },
+};
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Waits for the component to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId("loading"));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+  });
 };
 
 export const Error = Template.bind({});
